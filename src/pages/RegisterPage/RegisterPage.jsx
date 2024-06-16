e6dcc5b80a6a85cddafe2a5626c148fddf2d0c0f
YuliiaPro <129890065+YuliiaPro@users.noreply.github.com>
2024-06-16 21:29:02 +0300

    fixed error register

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,11 +1,19 @@
 import PageTitle from "../../components/PageTitle/PageTitle";
 import RegistrationForm from "../..//components/RegistrationForm/RegistrationForm";
+import Loader from "../../components/Loader/Loader";
+import ErrorLoginMessage from "../../components/ErrorLoginMessage/ErrorLoginMessage";
+import { useSelector } from "react-redux";
+import { selectError, selectLoading } from "../../redux/auth/selectors";
 
 export default function RegisterPage() {
+  const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
   return (
     <div>
       <PageTitle>Register your account</PageTitle>
       <RegistrationForm />
+      {isLoading && <Loader />}
+      {isError && <ErrorLoginMessage />}
     </div>
   );
 }
